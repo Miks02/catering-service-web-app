bcf7e944fd2cc385b43aaa291c44e05e0626130b
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-04 09:03:17 +0200

    initial

---
+++ src/main/java/org/example/catering/cateringserviceapp/CateringServiceAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.catering.cateringserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CateringServiceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/catering/cateringserviceapp/CateringServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.catering.cateringserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringServiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
