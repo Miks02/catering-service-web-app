a3d37a48be1ce13367d3d83e37b08cf09f932bb0
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-04 09:20:35 +0200

    feat(controller,view): Add a contact page and its corresponding controller

---
+++ src/main/java/org/example/catering/cateringserviceapp/controller/global/ContactController.java
@@ -0,0 +1,18 @@
+package org.example.catering.cateringserviceapp.controller.global;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/public")
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String showContactPage()
+    {
+        return "pages/public/contact";
+    }
+
+}
