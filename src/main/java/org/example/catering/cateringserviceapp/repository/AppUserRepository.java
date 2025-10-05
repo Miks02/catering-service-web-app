952453202f80a412ff750ed6e047397b7c1438f2
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-05 02:32:16 +0200

    feat(repository): Modify the return type of UserRepo so that they become Optional<>

--- src/main/java/org/example/catering/cateringserviceapp/repository/AppUserRepository.java
+++ src/main/java/org/example/catering/cateringserviceapp/repository/AppUserRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.example.catering.cateringserviceapp.models.AppUser;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser,Integer> {
 
-    public AppUser findUserByUsername(String username);
+    public Optional<AppUser> findUserByUsername(String username);
 
-    public AppUser findUserByEmail(String email);
+    public Optional<AppUser> findUserByEmail(String email);
 
 }
