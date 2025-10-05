f5b7e1d4048616631066042c3ff642b52f140cab
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-05 22:50:56 +0200

    feat(repository): Add ProductRepository with basic crud methods

---
+++ src/main/java/org/example/catering/cateringserviceapp/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package org.example.catering.cateringserviceapp.repository;
+
+import org.example.catering.cateringserviceapp.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    public Optional<Product> findProductById(Long id);
+    public Optional<Product> findProductByName(String name);
+    public List<Product> findAllProducts();
+    public void deleteProductById(Long id);
+
+}
