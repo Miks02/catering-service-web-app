bf367ed027241390325fa14857f532b33641fec2
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-05 02:33:26 +0200

    feat(controller): Add POST mapping for user registration with validation and error handling

--- src/main/java/org/example/catering/cateringserviceapp/controller/global/AccountController.java
+++ src/main/java/org/example/catering/cateringserviceapp/controller/global/AccountController.java
@@ -1,12 +1,32 @@
 package org.example.catering.cateringserviceapp.controller.global;
 
+import jakarta.validation.Valid;
+import org.example.catering.cateringserviceapp.exceptions.UserAlreadyExistsException;
+import org.example.catering.cateringserviceapp.models.AppUser;
+import org.example.catering.cateringserviceapp.service.AppUserService;
+import org.example.catering.cateringserviceapp.viewmodels.RegisterViewModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.sql.Date;
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/public")
 public class AccountController {
+
+    private final AppUserService userService;
+
+    public AccountController(AppUserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/login")
     public String login() {
@@ -14,8 +34,57 @@ public class AccountController {
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+
+        model.addAttribute("registerVm", new RegisterViewModel());
+
         return "pages/public/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("registerVm") RegisterViewModel registerVm,
+                           BindingResult result,
+                           Model model, RedirectAttributes redirectAttributes) {
+
+        if(result.hasErrors()) {
+            model.addAttribute("error", true);
+            return "pages/public/register";
+        }
+
+        if(!registerVm.getPassword().equals(registerVm.getConfirmPassword())) {
+            result.addError(new FieldError("registerVm", "confirmPassword", "Lozinke se ne poklapaju"));
+            return "pages/public/register";
+        }
+
+        try {
+            var user = new AppUser();
+
+            user.setUsername(registerVm.getUsername());
+            user.setPassword(registerVm.getPassword());
+            user.setEmail(registerVm.getEmail());
+            user.setFirstName(registerVm.getFirstName());
+            user.setLastName(registerVm.getLastName());
+            user.setCreatedAt(new Date(System.currentTimeMillis()));
+
+            userService.registerUser(user);
+            userService.loadUserByUsername(user.getUsername());
+
+        }
+        catch(UserAlreadyExistsException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+            return "redirect:/public/register";
+        }
+        catch(Exception e) {
+            model.addAttribute("error", true);
+            return "pages/public/register";
+        }
+
+
+        return "redirect:/public";
+
+
+
+
     }
 
 }
