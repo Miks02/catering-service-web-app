305af80c61a57b55d6ddb180ee2f52971704a746
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-06 12:44:45 +0200

    feat(repository): Make delete method transactional

--- src/main/java/org/example/catering/cateringserviceapp/repository/ProductRepository.java
+++ src/main/java/org/example/catering/cateringserviceapp/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package org.example.catering.cateringserviceapp.repository;
 
+import jakarta.transaction.Transactional;
 import org.example.catering.cateringserviceapp.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,6 +15,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     public Optional<Product> findProductById(Long id);
     public Optional<Product> findProductByName(String name);
 
+    @Transactional
     public void deleteProductById(Long id);
 
 }
