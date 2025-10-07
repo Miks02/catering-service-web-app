5f62b6e3a79095f3589bdb033dde219ea3f91055
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-07 10:46:35 +0200

    chore(config): Permit all users to access '/uploads/' folder

--- src/main/java/org/example/catering/cateringserviceapp/config/SecurityConfig.java
+++ src/main/java/org/example/catering/cateringserviceapp/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**", "/").permitAll()
-                        .requestMatchers("/css/**", "/images/**", "/js/**").permitAll()
+                        .requestMatchers("/css/**", "/images/**", "/js/**", "/uploads/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
