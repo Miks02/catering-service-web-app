204a3d71d36068e993e760a9950b3e4b51758ce3
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-07 04:11:39 +0200

    feat(js): Add functionality to show toast notification across all pages

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,3 +2,13 @@
 
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const toast = document.querySelector('.cart-toast');
+
+    setTimeout(() => {
+        toast.remove();
+    }, 3000);
+})
+
+
+
