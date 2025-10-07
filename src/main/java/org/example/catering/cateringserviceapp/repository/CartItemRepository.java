3124801911f0b99f3611df1dd4839129eb458a49
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-07 17:35:17 +0200

    feat(repository): Add @Repository annotations

--- src/main/java/org/example/catering/cateringserviceapp/repository/CartItemRepository.java
+++ src/main/java/org/example/catering/cateringserviceapp/repository/CartItemRepository.java
@@ -2,10 +2,12 @@ package org.example.catering.cateringserviceapp.repository;
 
 import org.example.catering.cateringserviceapp.models.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Optional<CartItem> FindByCartIdAndProductId(Long cartId, Long productId);
     List<CartItem> findByCartId(Long cartId);
--- src/main/java/org/example/catering/cateringserviceapp/repository/CartRepository.java
+++ src/main/java/org/example/catering/cateringserviceapp/repository/CartRepository.java
@@ -2,9 +2,10 @@ package org.example.catering.cateringserviceapp.repository;
 
 import org.example.catering.cateringserviceapp.models.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUserId(String userId);
 }
