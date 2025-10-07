457b64398b2c4444ad1d6a708eb56c9f5242818a
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-07 20:37:51 +0200

    feat(controller): Add a method for adding products to the cart in CartController

--- src/main/java/org/example/catering/cateringserviceapp/controller/user/CartController.java
+++ src/main/java/org/example/catering/cateringserviceapp/controller/user/CartController.java
@@ -1,16 +1,19 @@
 package org.example.catering.cateringserviceapp.controller.user;
 
+import org.example.catering.cateringserviceapp.dto.CartItemDTO;
 import org.example.catering.cateringserviceapp.models.AppUser;
 import org.example.catering.cateringserviceapp.models.Cart;
+import org.example.catering.cateringserviceapp.models.CartItem;
+import org.example.catering.cateringserviceapp.models.Product;
 import org.example.catering.cateringserviceapp.service.AppUserService;
 import org.example.catering.cateringserviceapp.service.CartService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Secured("ROLE_CLIENT")
@@ -39,9 +42,30 @@ public class CartController {
     public String cart(Model model) {
         Long userId = getUserId();
         Cart cart = cartService.getCart(userId);
-        System.out.println("Klijent ID: " + userId);
         model.addAttribute("cart", cart);
         return "pages/user/cart";
+    }
+
+    @PostMapping("/cart/add")
+    @ResponseBody
+    public ResponseEntity<?> addToCart(@RequestBody CartItemDTO cartItemDTO) {
+        try {
+            Long userId = getUserId();
+
+            CartItem newItem = new CartItem();
+            Product product = new Product();
+            product.setId(cartItemDTO.getProductId());
+
+            newItem.setProduct(product);
+            newItem.setQuantity(cartItemDTO.getQuantity());
+            newItem.setPrice(cartItemDTO.getPrice());
+
+            cartService.addToCart(userId, newItem);
+
+            return ResponseEntity.ok("Proizvod je dodat u korpu: " + cartItemDTO.getQuantity());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Greška prilikom dodavanja u korpu: " + e.getMessage());
+        }
     }
 
 
