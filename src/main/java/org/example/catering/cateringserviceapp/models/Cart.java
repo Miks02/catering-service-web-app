e5fa2c8e3eb9a5ea17024402f821da8c05f59a20
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-07 17:43:43 +0200

    feat(model): Add getters and setters in cart model

--- src/main/java/org/example/catering/cateringserviceapp/models/Cart.java
+++ src/main/java/org/example/catering/cateringserviceapp/models/Cart.java
@@ -2,6 +2,9 @@ package org.example.catering.cateringserviceapp.models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "carts")
 public class Cart {
@@ -11,6 +14,30 @@ public class Cart {
 
     private String userId;
 
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
 
+    public String getUserId() {
+        return userId;
+    }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
 }
