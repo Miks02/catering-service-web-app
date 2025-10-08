70d490be3b865f9008a5c3ef579ddfbeebb23034
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-09 01:32:35 +0200

    feat(service): Add another getAllUsers method for extracting all AppUsers

--- src/main/java/org/example/catering/cateringserviceapp/service/AppUserService.java
+++ src/main/java/org/example/catering/cateringserviceapp/service/AppUserService.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.*;
 import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -164,6 +165,10 @@ public class AppUserService implements UserDetailsService {
 
     public Page<AppUser> getAllUsers(Pageable pageable) {
         return userRepo.findAll(pageable);
+    }
+
+    public List<AppUser> getAllUsers() {
+        return userRepo.findAll();
     }
 
     public void deleteUser(Long id) {
