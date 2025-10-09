4a0c81deb8a85181cc6e6025600124fbc7cbfc50
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-10 00:24:33 +0200

    feat(repository): Add @Repository annotations

--- src/main/java/org/example/catering/cateringserviceapp/repository/OrderItemRepository.java
+++ src/main/java/org/example/catering/cateringserviceapp/repository/OrderItemRepository.java
@@ -2,6 +2,9 @@ package org.example.catering.cateringserviceapp.repository;
 
 import org.example.catering.cateringserviceapp.models.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+
 }
--- src/main/java/org/example/catering/cateringserviceapp/repository/OrderRepository.java
+++ src/main/java/org/example/catering/cateringserviceapp/repository/OrderRepository.java
@@ -2,7 +2,11 @@ package org.example.catering.cateringserviceapp.repository;
 
 import org.example.catering.cateringserviceapp.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
